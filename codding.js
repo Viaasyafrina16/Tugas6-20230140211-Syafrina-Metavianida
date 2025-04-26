77996bcceaf4cb62f88320fceafdbf7442533552
Viaasyafrina16 <syafrinavia@gmail.com>
2025-04-26 18:29:40 +0700

    first commit

---
+++ codding.js
@@ -0,0 +1,15 @@
+const video = document.getElementById('video');
+const audio = document.getElementById('audio');
+const gambar = document.getElementById('gambar');
+
+video.addEventListener("click", function() {
+    alert("Hai Ini video Trailer Jumbo");
+});
+
+audio.addEventListener("click", function() {
+    alert("Kalo ini Audio Aespa Whiplash");
+});
+
+gambar.addEventListener("click", function() {
+    alert("yang ini gambar aja");
+});
